1f6157f192214ba87f620bceb2fcfe99bc16c54b
AndreaMyshkn <atrkvsk@hotmail.com>
2019-06-12 14:29:47 -0500

    se arregla bug del cambio de contraseña

--- steam/src/Components/Account/index.js
+++ steam/src/Components/Account/index.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { AuthUserContext, withAuthorization } from '../Session'
 import { PasswordForgetForm } from '../PasswordForget'
 import PasswordChangeForm from '../PasswordChange'
+
 const AccountPage = () => (
   <AuthUserContext.Consumer>
     {authUser => (
       <div>
-        <h1>Account: {authUser.email}</h1>
+     <h1> Account: {authUser ? authUser.email : null}</h1>
         <PasswordForgetForm />
         <PasswordChangeForm />
       </div>
